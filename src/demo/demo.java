3c1c137325fe13cd60d1443b0a725c33ac73fc09
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 19:05:14 +0100

    cambio en master

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -5,7 +5,7 @@ public class demo {
 		/*cambio desde la luna*/
 		/*cambio 2 desde la luna en master*/
 		System.out.println("linea1");
-		System.out.println("linea2");
+		System.out.println("cambio en master ");
 		System.out.println("linea3");
 		/*Cambios en la rama 2*/
 	}
