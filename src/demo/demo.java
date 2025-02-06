f72224bca91bd8b8d49f13a2c37caa01bd4475ec
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 19:32:06 +0100

    Cambio para rebase solucionado

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -6,11 +6,7 @@ public class demo {
 		/*cambio 2 desde la luna en master*/
 		System.out.println("linea1");
 		System.out.println("cambio 3 luna master SOLUCIONADO");
-<<<<<<< Upstream, based on master
-		System.out.println("linea3 cambio para rebase");
-=======
-		System.out.println("linea cambio en la rama rebase");
->>>>>>> 1ec6261 cambio conflictivo en la rama rebase
+		System.out.println("linea3 cambio para rebase SOLUCIONADO");
 		/*Cambios en la rama 2*/
 	}
 
