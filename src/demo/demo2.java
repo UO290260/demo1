35413e13687f4ba681d3177ebf1be8062a3d8a20
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 20:23:40 +0100

    modificcion sin conflicto

--- src/demo/demo2.java
+++ src/demo/demo2.java
@@ -3,8 +3,10 @@ package demo;
 public class demo2 {
 
 	public static void main(String[] args) {
+		/*rama sin conflicto*/
 		// TODO Auto-generated method stub
 		/*camio en master*/
+	/*cambio sin conflicto en master */
 	}
 
 }
