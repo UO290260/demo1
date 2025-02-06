ffdedba6f809e4fb26207cffa7f56bccbb1824d6
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 18:31:40 +0100

    cambio 2 desde la luna en master

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -3,6 +3,7 @@ package demo;
 public class demo {
 	public static void main(String[] args) {
 		/*cambio desde la luna*/
+		/*cambio 2 desde la luna en master*/
 		System.out.println("linea1");
 		System.out.println("linea2");
 		System.out.println("linea3");
