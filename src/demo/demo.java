62c18856d78d9bc8ae1343e88c1fc194a179af4a
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 17:57:30 +0100

    Cambios en el fichero existente

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -1,5 +1,10 @@
 package demo;
 
 public class demo {
+	public static void main(String[] args) {
+		System.out.println("linea1");
+		System.out.println("linea1");
+		System.out.println("linea1");
+	}
 
 }
