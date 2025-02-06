ad2c8edb00e9c2822b37aa50ce63f29678a25f65
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 20:06:32 +0100

    cambio desde la luna en master

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -3,10 +3,10 @@ package demo;
 public class demo {
 	public static void main(String[] args) {
 		/*cambio desde la luna*/
-		/*cambio 2 desde la luna en master*/
+		/*cambio desde la luna en master*/
 		System.out.println("linea1");
-		System.out.println("cambio 3 luna master SOLUCIONADO");
-		System.out.println("linea3 cambio para rebase SOLUCIONADO");
+		System.out.println("linea 2");
+		System.out.println("linea3");
 		/*Cambios en la rama 2*/
 	}
 
