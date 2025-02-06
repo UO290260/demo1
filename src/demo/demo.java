65c0663560e5d14ccfeda3851c940a842e91897e
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-02-06 18:16:54 +0100

    Cambio desde la luna

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -2,6 +2,7 @@ package demo;
 
 public class demo {
 	public static void main(String[] args) {
+		/*cambio desde la luna*/
 		System.out.println("linea1");
 		System.out.println("linea2");
 		System.out.println("linea3");
